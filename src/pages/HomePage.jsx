bb0f295f8ffdb3650caa78cb7b19fa87ccb1c0e3
Vivek Nair <vivek@nair.me>
2021-11-29 17:44:11 -0800

    refactor

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 
 import logo from '../images/logo.svg';
 import cm from '../images/cm.svg';
@@ -7,104 +6,8 @@ import qm from '../images/qm.svg';
 import bm from '../images/bm.svg';
 import sm from '../images/sm.svg';
 import lm from '../images/lm.svg';
-import { Link } from 'react-router-dom';
 
-class Leaderboard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  async componentDidMount() {
-    const data = await axios.get('https://api.beatquest.com/leaderboard');
-    this.setState({ data: data.data.data });
-  }
-
-  render() {
-    if (this.state.data) {
-      const players = this.state.data.map((player, i) => {
-        var delta = <div className="delta text-secondary small">⬤ 0</div>;
-        if (player.delta > 0) {
-          delta = (
-            <div className="delta text-success small">▲ {player.delta}</div>
-          );
-        } else if (player.delta < 0) {
-          delta = (
-            <div className="delta text-danger small">▼ {player.delta}</div>
-          );
-        }
-        var color = {
-          CM: '#e47d22',
-          QM: '#cfd2d3',
-          BM: '#f0f328',
-          SM: '#a0ddd1',
-          LM: '#51ebfd',
-        }[player.rank];
-        var icon = {
-          CM: cm,
-          QM: qm,
-          BM: bm,
-          SM: sm,
-          LM: lm,
-        }[player.rank];
-        return (
-          <tr key={player.id}>
-            <td>{i + 1}</td>
-            <td>
-              {player.elo}
-              {player.matches < 5 && <> (??)</>}
-              {delta}
-            </td>
-            <td>
-              <img src={player.image} alt={player.name} className="profile" />
-              {<Link to={`/user/${player.id}`}>{player.name}</Link>}
-              {player.rank !== 'N' && (
-                <>
-                  <br />
-                  <span
-                    className="badge badge-secondary"
-                    style={{ backgroundColor: color }}
-                  >
-                    <span class="icon">
-                      <img alt={player.rank} src={icon} />
-                    </span>{' '}
-                    {player.rank}
-                  </span>
-                </>
-              )}
-            </td>
-            <td>
-              {player.matches} Matches
-              <br />
-              <span className="text-muted">
-                {player.wins + player.losses} Maps
-              </span>
-            </td>
-            <td>
-              {player.wins}/{player.losses}
-              <br />
-              <span className="text-muted">{player.norms} Norms</span>
-            </td>
-          </tr>
-        );
-      });
-      return (
-        <table className="table table-dark mt-5">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Rating</th>
-              <th scope="col">Player</th>
-              <th scope="col">Matches</th>
-              <th scope="col">W/L</th>
-            </tr>
-          </thead>
-          <tbody>{players}</tbody>
-        </table>
-      );
-    } else return <></>;
-  }
-}
+import Leaderboard from '../components/Leaderboard';
 
 function HomePage() {
   var color = {
@@ -122,78 +25,84 @@ function HomePage() {
     LM: lm,
   };
 
-  return (
+  return (<>
+    <div className="row mt-5 pt-5">
+      <div className="col-3">
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+      <div className="col-9 pt-4 mt-3">
+        <p><b>Beat Saber Competitive Rankings</b></p>
+        <p className="mb-0">
+          <span
+            className="badge badge-secondary"
+            style={{ backgroundColor: color.CM }}
+          >
+            <span class="icon">
+              <img alt="CM" src={icon.CM} />
+            </span>
+            CM
+          </span>&nbsp;
+          Candidate Master: Has played 5+ matches and 1500+ peak elo
+        </p>
+        <p className="mb-0">
+          <span
+            className="badge badge-secondary"
+            style={{ backgroundColor: color.QM }}
+          >
+            <span class="icon">
+              <img alt="CM" src={icon.QM} />
+            </span>
+            QM
+          </span>&nbsp;
+          Quest Master: Has played 5+ matches and 1800+ peak elo
+        </p>
+        <p className="mb-0">
+          <span
+            className="badge badge-secondary"
+            style={{ backgroundColor: color.BM }}
+          >
+            <span class="icon">
+              <img alt="CM" src={icon.BM} />
+            </span>
+            BM
+          </span>&nbsp;
+          Beat Master: Has played 5+ matches and 2000+ peak elo
+        </p>
+        <p className="mb-0">
+          <span
+            className="badge badge-secondary"
+            style={{ backgroundColor: color.SM }}
+          >
+            <span class="icon">
+              <img alt="CM" src={icon.SM} />
+            </span>
+            SM
+          </span>&nbsp;
+          Saber Master: Has 2200+ peak elo and 5+ norm matches
+        </p>
+        <p className="mb-0">
+          <span
+            className="badge badge-secondary"
+            style={{ backgroundColor: color.LM }}
+          >
+            <span class="icon">
+              <img alt="CM" src={icon.LM} />
+            </span>
+            LM
+          </span>&nbsp;
+          Legendary Master: Has achieved SM and 2500+ peak elo
+        </p>
+      </div>
+    </div>
     <div className="App">
       <header className="App-header">
         <div className="text-center">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p><b>Beat Saber Competitive Rankings</b></p>
-          <br />
-          <p className="mb-0">
-            <span
-              className="badge badge-secondary"
-              style={{ backgroundColor: color.CM }}
-            >
-              <span class="icon">
-                <img alt="CM" src={icon.CM} />
-              </span>
-              CM
-            </span>&nbsp;
-            Candidate Master: Has played 5+ matches and 1500+ peak elo
-          </p>
-          <p className="mb-0">
-            <span
-              className="badge badge-secondary"
-              style={{ backgroundColor: color.QM }}
-            >
-              <span class="icon">
-                <img alt="CM" src={icon.QM} />
-              </span>
-              QM
-            </span>&nbsp;
-            Quest Master: Has played 5+ matches and 1800+ peak elo
-          </p>
-          <p className="mb-0">
-            <span
-              className="badge badge-secondary"
-              style={{ backgroundColor: color.BM }}
-            >
-              <span class="icon">
-                <img alt="CM" src={icon.BM} />
-              </span>
-              BM
-            </span>&nbsp;
-            Beat Master: Has played 5+ matches and 2000+ peak elo
-          </p>
-          <p className="mb-0">
-            <span
-              className="badge badge-secondary"
-              style={{ backgroundColor: color.SM }}
-            >
-              <span class="icon">
-                <img alt="CM" src={icon.SM} />
-              </span>
-              SM
-            </span>&nbsp;
-            Saber Master: Has 2200+ peak elo and 5+ norm matches
-          </p>
-          <p className="mb-0">
-            <span
-              className="badge badge-secondary"
-              style={{ backgroundColor: color.LM }}
-            >
-              <span class="icon">
-                <img alt="CM" src={icon.LM} />
-              </span>
-              LM
-            </span>&nbsp;
-            Legendary Master: Has achieved SM and 2500+ peak elo
-          </p>
+
         </div>
         <Leaderboard />
       </header>
     </div>
-  );
+  </>);
 }
 
 export default HomePage;
--- src/pages/LeaderboardPage.jsx
+++ src/pages/LeaderboardPage.jsx
@@ -1,111 +1,7 @@
 import React from 'react';
-import axios from 'axios';
+import Leaderboard from '../components/Leaderboard';
 
-import cm from '../images/cm.svg';
-import qm from '../images/qm.svg';
-import bm from '../images/bm.svg';
-import sm from '../images/sm.svg';
-import lm from '../images/lm.svg';
-import { Link } from 'react-router-dom';
-
-class Leaderboard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  async componentDidMount() {
-    const data = await axios.get('https://api.beatquest.com/leaderboard');
-    this.setState({ data: data.data.data });
-  }
-
-  render() {
-    if (this.state.data) {
-      const players = this.state.data.map((player, i) => {
-        var delta = <div className="delta text-secondary small">⬤ 0</div>;
-        if (player.delta > 0) {
-          delta = (
-            <div className="delta text-success small">▲ {player.delta}</div>
-          );
-        } else if (player.delta < 0) {
-          delta = (
-            <div className="delta text-danger small">▼ {player.delta}</div>
-          );
-        }
-        var color = {
-          CM: '#e47d22',
-          QM: '#cfd2d3',
-          BM: '#f0f328',
-          SM: '#a0ddd1',
-          LM: '#51ebfd',
-        }[player.rank];
-        var icon = {
-          CM: cm,
-          QM: qm,
-          BM: bm,
-          SM: sm,
-          LM: lm,
-        }[player.rank];
-        return (
-          <tr key={player.id}>
-            <td>{i + 1}</td>
-            <td>
-              {player.elo}
-              {player.matches < 5 && <> (??)</>}
-              {delta}
-            </td>
-            <td>
-              <img src={player.image} alt={player.name} className="profile" />
-              {<Link to={`/user/${player.id}`}>{player.name}</Link>}
-              {player.rank !== 'N' && (
-                <>
-                  <br />
-                  <span
-                    className="badge badge-secondary"
-                    style={{ backgroundColor: color }}
-                  >
-                    <span class="icon">
-                      <img alt={player.rank} src={icon} />
-                    </span>{' '}
-                    {player.rank}
-                  </span>
-                </>
-              )}
-            </td>
-            <td>
-              {player.matches} Matches
-              <br />
-              <span className="text-muted">
-                {player.wins + player.losses} Maps
-              </span>
-            </td>
-            <td>
-              {player.wins}/{player.losses}
-              <br />
-              <span className="text-muted">{player.norms} Norms</span>
-            </td>
-          </tr>
-        );
-      });
-      return (
-        <table className="table table-dark mt-5">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Rating</th>
-              <th scope="col">Player</th>
-              <th scope="col">Matches</th>
-              <th scope="col">W/L</th>
-            </tr>
-          </thead>
-          <tbody>{players}</tbody>
-        </table>
-      );
-    } else return <></>;
-  }
-}
-
-function HomePage() {
+function LeaderboardPage() {
   return (
     <div className="App">
       <header className="App-header">
@@ -116,4 +12,4 @@ function HomePage() {
   );
 }
 
-export default HomePage;
+export default LeaderboardPage;
