d5e4e9ffdbe8f87127901d84f0fa7d5d77effff2
Vivek Nair <vivek@nair.me>
2021-11-29 17:27:41 -0800

    upgraded navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link, NavLink } from 'react-router-dom';
 import { EventPage } from './pages/event/EventPage';
 import { EventsPage } from './pages/events/EventsPage';
 import HomePage from './pages/HomePage';
 import UserPage from './pages/UserPage';
+import LeaderboardPage from './pages/LeaderboardPage';
+import logo from './images/icon.svg';
 
 function App() {
-  return (
-    <Routes>
-      <Route exact path="/" element={<HomePage />} />
-      <Route path="/user/:name" element={<UserPage />} />
-      <Route path="/events" element={<EventsPage />} />
-      <Route path="/event/:id" element={<EventPage />} />
-    </Routes>
-  );
+  return (<>
+    <nav className="navbar navbar-dark bg-dark fixed-top navbar-expand-sm">
+      <div className="container">
+        <Link to="/" className="navbar-brand">
+          <img src={logo} alt="BQ" height="28" class="d-inline-block align-text-top" />
+          &nbsp; BeatQuest
+        </Link>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <NavLink to="/" className="nav-link">Home</NavLink>
+            </li>
+              <li class="nav-item">
+                <NavLink to="/leaderboard" className="nav-link">Leaderboard</NavLink>
+              </li>
+            <li class="nav-item">
+              <NavLink to="/events" className="nav-link">Events</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div className="container beatquest-main mt-5">
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/leaderboard" element={<LeaderboardPage />} />
+        <Route path="/user/:name" element={<UserPage />} />
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/event/:id" element={<EventPage />} />
+      </Routes>
+    </div>
+  </>);
 }
 
 export default App;
--- src/pages/LeaderboardPage.jsx
+++ src/pages/LeaderboardPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import axios from 'axios';
 
-import logo from '../images/logo.svg';
 import cm from '../images/cm.svg';
 import qm from '../images/qm.svg';
 import bm from '../images/bm.svg';
@@ -107,89 +106,10 @@ class Leaderboard extends React.Component {
 }
 
 function HomePage() {
-  var color = {
-    CM: '#e47d22',
-    QM: '#cfd2d3',
-    BM: '#f0f328',
-    SM: '#a0ddd1',
-    LM: '#51ebfd',
-  };
-  var icon = {
-    CM: cm,
-    QM: qm,
-    BM: bm,
-    SM: sm,
-    LM: lm,
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-        <div className="text-center">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p><b>Beat Saber Competitive Rankings</b></p>
-          <br />
-          <p className="mb-0">
-            <span
-              className="badge badge-secondary"
-              style={{ backgroundColor: color.CM }}
-            >
-              <span class="icon">
-                <img alt="CM" src={icon.CM} />
-              </span>
-              CM
-            </span>&nbsp;
-            Candidate Master: Has played 5+ matches and 1500+ peak elo
-          </p>
-          <p className="mb-0">
-            <span
-              className="badge badge-secondary"
-              style={{ backgroundColor: color.QM }}
-            >
-              <span class="icon">
-                <img alt="CM" src={icon.QM} />
-              </span>
-              QM
-            </span>&nbsp;
-            Quest Master: Has played 5+ matches and 1800+ peak elo
-          </p>
-          <p className="mb-0">
-            <span
-              className="badge badge-secondary"
-              style={{ backgroundColor: color.BM }}
-            >
-              <span class="icon">
-                <img alt="CM" src={icon.BM} />
-              </span>
-              BM
-            </span>&nbsp;
-            Beat Master: Has played 5+ matches and 2000+ peak elo
-          </p>
-          <p className="mb-0">
-            <span
-              className="badge badge-secondary"
-              style={{ backgroundColor: color.SM }}
-            >
-              <span class="icon">
-                <img alt="CM" src={icon.SM} />
-              </span>
-              SM
-            </span>&nbsp;
-            Saber Master: Has 2200+ peak elo and 5+ norm matches
-          </p>
-          <p className="mb-0">
-            <span
-              className="badge badge-secondary"
-              style={{ backgroundColor: color.LM }}
-            >
-              <span class="icon">
-                <img alt="CM" src={icon.LM} />
-              </span>
-              LM
-            </span>&nbsp;
-            Legendary Master: Has achieved SM and 2500+ peak elo
-          </p>
-        </div>
+        <h1>Leaderboard</h1>
         <Leaderboard />
       </header>
     </div>
